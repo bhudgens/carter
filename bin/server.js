5dfb88f993db91002d2de13d39656050334d5cf0
Benjamin Hudgens <benjamin@benjamindavid.com>
2020-07-19 16:05:09 -0500

    All the things for carter

--- bin/server.js
+++ bin/server.js
@@ -1,6 +1,7 @@
 /** Hookup Express */
 const express = require("express");
 const app = express();
+const log = require("../src/lib/log.js").init('servers.js');
 
 /** Configure our body Parser */
 const bodyParser = require("body-parser");
@@ -12,9 +13,22 @@ app.use(bodyParser.urlencoded({ extended: true }));
  ********************************************************************/
 
 app.use("/", express.static(`${__dirname}/../public`));
+app.get("/codes/:password", (req, res) => {
+  const { password } = req.params;
+  if (password !== 1234) {
+    const error = "You do not have the correct password";
+    log.red(`Password provided ${password}`);
+    return res.status(403).json({ error });
+  }
+  const message = `It worked: ${req.params.password}`;
+  res.status(200).json({ message });
+});
 
 /********************************************************************
  * Start the Express Server
  ********************************************************************/
-app.listen(process.env.PORT || 3000);
+const { PORT } = process.env;
+const _port = PORT || 3000;
+app.listen(_port, () => log.green(`Running server on port ${_port}`));
 
+log.blue("Hello Carter");
